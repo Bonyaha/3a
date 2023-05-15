964f1ec2927ecb256620321de379754647979a16
Roman <romchikus1987@gmail.com>
2023-05-15 18:16:50 +0300

    updated

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 const express = require('express')
 const app = express()
+
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
 app.use(express.json())
+app.use(requestLogger)
+
 let notes = [
   { id: 1, content: 'HTML is easy', important: true },
   { id: 2, content: 'Browser can execute only JavaScript', important: false },
@@ -10,6 +25,10 @@ let notes = [
     important: true,
   },
 ]
+const generatedId = () => {
+  const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0
+  return maxId + 1
+}
 
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
@@ -28,17 +47,13 @@ app.get('/api/notes/:id', (request, response) => {
 })
 app.delete('/api/notes/:id', (request, response) => {
   const id = Number(request.params.id)
-  const note = notes.find((note) => note.id === id)
+  const notes = notes.filter((note) => note.id !== id)
   response.status(204).end()
 })
 app.post('/api/notes', (request, response) => {
   //console.log(request.headers)
   const body = request.body
   //console.log(note)
-  const generatedId = () => {
-    const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0
-    return maxId + 1
-  }
   if (!body.content) {
     return response.status(400).json({
       error: 'content is missing',
@@ -53,6 +68,8 @@ app.post('/api/notes', (request, response) => {
   notes = notes.concat(note)
   response.json(note)
 })
+
+app.use(unknownEndpoint)
 
 const PORT = 3001
 app.listen(PORT)
