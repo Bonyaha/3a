3b1370d730653b5fe27cb7ab83c1204d4942fe28
Roman <romchikus1987@gmail.com>
2023-05-19 13:36:18 +0300

    new branch 3c

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
-//app.use(express.static('build')) //To make express show static content, the page index.html
+app.use(express.static('build')) //To make express show static content, the page index.html
 
 const requestLogger = (request, response, next) => {
   console.log('Method:', request.method)
